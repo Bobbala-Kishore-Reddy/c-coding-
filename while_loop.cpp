c66e48d603862a82a5aae3378fc158c056530ad4
Bobbala-Kishore-Reddy <iambobbala@gmail.com>
2025-11-05 19:25:37 +0530

    Create while_loop.cpp

---
+++ while_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 1;
+    cout << "Enter the range: ";
+    cin >> n;
+
+    while (i <= n) {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}
